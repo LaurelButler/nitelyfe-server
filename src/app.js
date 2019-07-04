c293ffabac3d2b8db0f3f65b0288f4deaba68f7c
LaurelButler <laurelbutler126@gmail.com>
2019-07-03 23:27:03 -0400

    access control

--- src/app.js
+++ src/app.js
@@ -15,6 +15,15 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
+    if (req.method === "OPTIONS") {
+        return res.send(204);
+    }
+    next();
+});
 
 app.use(eventsRouter);
 
