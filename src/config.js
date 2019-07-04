05680610829f070af12aaa37fb977e19815c81e1
LaurelButler <laurelbutler126@gmail.com>
2019-07-03 23:10:18 -0400

    working on delete for heroku

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL|| 'postgresql://laurelbutler:@localhost/nitelyfe',
+    DB_URL: process.env.DATABASE_URL || 'postgres://lndvpkchucyyfp:bede3024e2d124333a85ece8104531dc9d93f0a9da7b99b2f310b33e3f6d4777@ec2-107-20-230-70.compute-1.amazonaws.com:5432/d1vme47sm5ujl8',
 }
